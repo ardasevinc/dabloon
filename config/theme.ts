a342b1d4a3e6f9dc366a00ca0ba363bcc73dfa93
Arda Sevinç <ardasevinc.technical@gmail.com>
2022-12-30 03:22:11 +0300

    feat: customize chakra foundation theme

--- config/theme.ts
+++ config/theme.ts
@@ -1,12 +1,43 @@
 interface Theme {
   config: {
-    initialColorMode: "light" | "dark" | "system" | undefined;
+    initialColorMode: 'light' | 'dark' | 'system' | undefined;
+    useSystemColorMode: boolean;
+  };
+  colors: {
+    bg: string;
+    text: string;
+    para: string;
+    button: string;
+  };
+  styles: {
+    global: {
+      body: {
+        bg: string;
+        color: string;
+      };
+    };
   };
 }
 
 const theme: Theme = {
   config: {
-    initialColorMode: "dark",
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+  colors: {
+    bg: '#0f0e17',
+    text: '#fffffe',
+    para: '#a7a9be',
+    button: '#ff8906',
+  },
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: 'bg',
+        color: 'text',
+      },
+    },
   },
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import Head from "next/head";
+import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Head from 'next/head';
+import theme from '../config/theme';
+
+const themeExtended = extendTheme({
+  colors: theme.colors,
+  styles: theme.styles,
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={themeExtended}>
       <Head>
         <title>Next.js + Chakra UI</title>
       </Head>
